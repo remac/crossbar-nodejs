daeedfd47fa55bc62bb411c9ab7c113f3764ee7a
Brad Gardner <bradgardner@sevenhillstechnology.com>
2019-02-27 14:12:13 -0500

    added existence tests and added cdrs

--- tests/crossbar.spec.js
+++ tests/crossbar.spec.js
@@ -16,4 +16,40 @@ describe('Crossbar Tests', () => {
             done(err);
         }
     });
+
+    it ('Should contain accounts api', () => {
+        const cb = Crossbar();
+
+        expect(cb.api.accounts, `Accounts api doesn't exist`).to.exist;
+    });
+
+    it ('Should contain devices api', () => {
+        const cb = Crossbar();
+
+        expect(cb.api.devices, `devices api doesn't exist`).to.exist;
+    });
+
+    it ('Should contain faxes api', () => {
+        const cb = Crossbar();
+
+        expect(cb.api.faxes, `faxes api doesn't exist`).to.exist;
+    });
+
+    it ('Should contain user_auth api', () => {
+        const cb = Crossbar();
+
+        expect(cb.api.user_auth, `user_auth api doesn't exist`).to.exist;
+    });
+
+    it ('Should contain users api', () => {
+        const cb = Crossbar();
+
+        expect(cb.api.users, `users api doesn't exist`).to.exist;
+    });
+
+    it ('Should contain cdrs api', () => {
+        const cb = Crossbar();
+
+        expect(cb.api.cdrs, `CDR api doesn't exist`).to.exist;
+    });
 });
